5dae14e6ba3efdb6ad1b170d039bbb861f76e2a2
a3490620 <197717697+a3490620@users.noreply.github.com>
2025-04-04 15:28:18 +0700

    feat: make Node variables private and add getter and setter for them

--- Node.js
+++ Node.js
@@ -1,6 +1,25 @@
 export default class Node {
+	#value;
+	#nextNode;
+
 	constructor(value = null, nextNode = null) {
-		this.value = value;
-		this.nextNode = nextNode;
+		this.#value = value;
+		this.#nextNode = nextNode;
+	}
+
+	get value() {
+		return this.#value;
+	}
+
+	set value(newValue) {
+		this.#value = newValue;
+	}
+
+	get nextNode() {
+		return this.#nextNode;
+	}
+
+	set nextNode(newNextNode) {
+		this.#nextNode = newNextNode;
 	}
 }
